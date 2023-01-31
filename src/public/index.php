6c09c4b062587a67779042dc90629786e6e02b65
6-ON <aminhatim6@gmail.com>
2023-01-31 21:01:56 +0100

    update improve ROOT_DIR

--- src/public/index.php
+++ src/public/index.php
@@ -1,13 +1,14 @@
 <?php
-$ROOT_DIR = dirname(dirname(__DIR__));
+$ROOT_DIR = dirname(__DIR__, 2);
 require_once $ROOT_DIR .'/vendor/autoload.php';
 use sixon\hwFramework\Application;
+use Yc\CineHallBackend\controllers\ApiController;
 
 
 $dotenv = Dotenv\Dotenv::createImmutable($ROOT_DIR);
 $dotenv->load();
 $config = [
-    'userClass' => User::class,
+    'userClass' => Yc\CineHallBackend\models\User::class,
     'db' => [
         'dsn' => $_ENV['DB_DSN'],
         'usr' => $_ENV['DB_USR'],
@@ -16,3 +17,14 @@ $config = [
     ]
 ];
 $app = new Application($ROOT_DIR,$config);
+
+
+$app->router->put('/users/create',[ApiController::class,'test']);
+
+
+
+
+
+
+
+$app->run();
