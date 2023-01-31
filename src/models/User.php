c1bd538604b16355fe839c07bc37117fe6838b35
6-ON <aminhatim6@gmail.com>
2023-01-31 16:42:58 +0100

    create User Model

--- src/models/User.php
+++ src/models/User.php
@@ -2,7 +2,49 @@
 
 namespace Yc\CineHallBackend\models;
 
-class User
+use sixon\hwFramework\UserModel;
+
+class User extends UserModel
 {
 
+    public $email;
+    public $firstName;
+    public $lastName;
+    public $token;
+    public $image;
+
+    public static function tableName(): string
+    {
+        return 'user';
+    }
+
+    public function attributes(): array
+    {
+        return ['firstName', 'lastName', 'token', 'image'];
+    }
+
+    public static function primaryKey(): string
+    {
+        return 'id';
+    }
+
+    public function rules(): array
+    {
+        return ['firstName', 'lastName', 'token', 'image'];
+    }
+
+    public function labels(): array
+    {
+        return [];
+    }
+
+    public function getDisplayName(): string
+    {
+        return $this->firstName . ' ' . $this->lastName;
+    }
+
+    public function getDisplayEmail(): string
+    {
+        return $this->email;
+    }
 }
